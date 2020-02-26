31afac89115431566bcfb31f51bbadbc2b306d9d
Arnav <arnavpanwar99@gmail.com>
2020-02-26 14:36:53 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,40 @@
+const hbs = require('hbs');
+const express = require('express');
+
+const app = express();
+
+hbs.registerPartials(`${__dirname}/views/partials`);
+app.set('view engine', 'hbs');
+app.use(express.static(`${__dirname}/public`));
+
+app.use((req, res, next) => {
+    const now = new Date().toString();
+    console.log(`${now}: ${req.method} ${req.url}`);
+    next();
+});
+
+hbs.registerHelper('getCurrentYear', () => new Date().getFullYear())
+
+hbs.registerHelper('screamIt', (text) => text.toUpperCase())
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        user: 'Arnav' 
+    })
+})
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+    });
+})
+
+app.get('/bad', (req, res) => {
+    res.send({
+        error: {
+            errorMessage: 'Not a valid route'
+        }
+    })
+})
+
+app.listen(3000, () => console.log('server is up on 3000'));
